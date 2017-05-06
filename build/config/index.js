abe983beee5c884fdb6a52c3087aec31d9cc6107
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2017-05-06 14:34:15 +0900

    build: add to serve public contents

--- build/config/index.js
+++ build/config/index.js
@@ -3,3 +3,4 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const path_1 = require("path");
 exports.SERVER_ROOT = path_1.join(__dirname, '..');
 exports.PROJECT_ROOT = path_1.join(__dirname, '../..');
+exports.PUBLIC_ROOT = path_1.join(__dirname, '../../public');
