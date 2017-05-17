c9424ec07fb5d0087a1929107f4b5010e1f28439
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2017-05-17 19:18:22 +0900

    feat(lib): implement function `fetchHtml` and refactor `getHtmlOfTokyuBus`

--- server/lib/get-html-of-tokyu-bus.ts
+++ server/lib/get-html-of-tokyu-bus.ts
@@ -1,4 +1,5 @@
-import * as http from 'http';
+import { HttpResult } from '../types/index';
+import { fetchHtml } from './fetch-html';
 
 /**
  * # About URL of tokyu.bus-location
@@ -29,41 +30,6 @@ const urlOfTokyuBus = 'http://tokyu.bus-location.jp/blsys/navi?VID=rsl&EID=nt&PR
 // const urlOfTokyuBusNavi = 'http://tokyu.bus-location.jp/blsys/navi?VID=lsc&EID=nt&SCT=2&DSMK=2598&ASMK=2336';
 
 
-interface ResultOfGetHtmlOfTokyuBus {
-  status: number;
-  header: any;
-  contents: string;
-  error?: any;
-}
-
-export function getHtmlOfTokyuBus(): Promise<ResultOfGetHtmlOfTokyuBus> {
-  return new Promise((resolve, reject) => {
-    const result: ResultOfGetHtmlOfTokyuBus = {
-      status: 0,
-      header: {},
-      contents: ''
-    };
-
-    const req = http.get(urlOfTokyuBus, (res) => {
-      result.status = res.statusCode || 400;
-      result.header = res.headers;
-
-      const bodyChunk: Buffer[] = [];
-      res.on('data', (chunk: Buffer) => {
-        bodyChunk.push(chunk);
-      });
-
-      res.on('end', () => {
-        const body = Buffer.concat(bodyChunk);
-        result.contents = body.toString();
-
-        resolve(result);
-      });
-    });
-
-    req.on('error', (err) => {
-      result.error = err;
-      reject(result);
-    });
-  });
+export function getHtmlOfTokyuBus(): Promise<HttpResult> {
+  return fetchHtml(urlOfTokyuBus);
 }
