fd60bb35180fb397a8bcf31d13138cc35baf1666
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2017-05-17 20:44:02 +0900

    refactor: log directory location

--- server/config/index.ts
+++ server/config/index.ts
@@ -2,4 +2,5 @@ import { join } from 'path';
 
 export const SERVER_ROOT = join(__dirname, '..');
 export const PROJECT_ROOT = join(__dirname, '../..');
-export const PUBLIC_ROOT = join(__dirname, '../../public');
+export const PUBLIC_ROOT = join(PROJECT_ROOT, 'public/');
+export const LOG_ROOT = join(PROJECT_ROOT, 'log/');
