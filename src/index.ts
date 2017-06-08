b26c812a959ce5753f57730c313811f1299bbca0
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2017-06-08 17:28:49 +0900

    feat: add kcors
    For Cross-Origin Resource Sharing

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import * as Koa from 'koa';
 import * as koaBodyparser from 'koa-bodyparser';
 import * as koaStatic from 'koa-static';
+import * as kcors from 'kcors';
 import { rootRouter } from './routers';
 import { PUBLIC_ROOT } from './config/index';
 import { processTimeLogger } from './middlewares/process-time-logger';
 import { logger } from './lib/logger';
 
 const app = new Koa();
+
+/**
+ * Update header for Cross-Origin Resource Sharing
+ */
+app.use(kcors());
 
 app.use(koaStatic(PUBLIC_ROOT));
 app.use(koaBodyparser());
