4da898df57b32d6a0c8250e8e3cca1e8529ce649
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2017-06-23 21:50:43 +0900

    feat: add a new function and its test file

---
+++ src/lib/parse-tokyu-bus-location-html.spec.ts
@@ -0,0 +1,8 @@
+import { parseTokyuBusLocationHtml } from './parse-tokyu-bus-location-html';
+
+
+describe('parseTokyuBusLocationHtml', () => {
+  it('should be return test text', () => {
+    expect(parseTokyuBusLocationHtml()).toBe('parseTokyuBusLocationHtml');
+  });
+});
