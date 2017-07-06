ca59c61055500c698d6159bca9e7ccfaf426a0fb
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2017-07-06 23:10:56 +0900

    refactor: move sample text to new file as mock

--- src/lib/tokyu-bus-timetable/fetch-bus-stop-name-search-html.spec.ts
+++ src/lib/tokyu-bus-timetable/fetch-bus-stop-name-search-html.spec.ts
@@ -1,10 +1,12 @@
 import { fetchBusStopNameSearchHTML } from './fetch-bus-stop-name-search-html';
+import { busStopNameSearchHTMLMockString } from './bus-stop-name-search-html.mock';
+
 
 describe('fetchBusStopNameSearchHTML', () => {
-  fit('should be same result to sample-html', (done) => {
+  it('should be same result to sample-html', (done) => {
     fetchBusStopNameSearchHTML('下馬')
       .then((result) => {
-        const expected = sample;
+        const expected = busStopNameSearchHTMLMockString;
         const actual = result.contents;
 
         // date is variable, so compare with length of each html.
@@ -15,37 +17,3 @@ describe('fetchBusStopNameSearchHTML', () => {
       });
   });
 });
-
-
-
-const sample = `
-<html><head>
-<meta http-equiv="Content-Type" content="text/html; charset=shift_jis">
-<meta http-equiv="Pragma" content="no-cache">
-<meta http-equiv="Cache-Control" content="no-cache">
-<meta http-equiv="Expires" content="-1">
-<title></title>
-</head><body>
-
-<center>
-＝バス時刻表＝
-</center>
-
-<hr>
-<br>
-
-▼停留所名称検索<br>
-[下馬]の検索結果(4件)<br>
-
-<a href="./index.cgi?cd=11121303,07/06,7,2,%89%ba%94n,1,,,,,,1710409,">下馬一丁目</a><br>
-<a href="./index.cgi?cd=11121303,07/06,7,2,%89%ba%94n,1,,,,,,1710410,">下馬営業所</a><br>
-<a href="./index.cgi?cd=11121303,07/06,7,2,%89%ba%94n,1,,,,,,1735415,">下馬五丁目</a><br>
-<a href="./index.cgi?cd=11121303,07/06,7,2,%89%ba%94n,1,,,,,,1725509,">下馬六丁目</a><br>
-
-<br>
-停留所を選択してください。<br>
-<br>
-<hr>
-<a href="./index.cgi?cd=1112,07/06," accesskey="9">9. 戻る</a><br>
-<a href="./index.cgi" accesskey="0">0. ﾄｯﾌﾟへ</a><br>
-</body></html>`;
