f7c4fa2f305a83fc17e87d257ec73389f3aabcfa
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2017-07-08 11:37:06 +0900

    feat: implement APIs of timetable

--- src/routers/api/timetable/index.ts
+++ src/routers/api/timetable/index.ts
@@ -1,5 +1,12 @@
 import * as Router from 'koa-router';
 import { makeWebApiErrorResultObject } from '../../../lib/make-web-api-error-result-object';
+import { fetchBusStopNameSearchHTML } from '../../../lib/tokyu-bus-timetable/bus-stop-name-search-html/fetch-bus-stop-name-search-html';
+import { parseForQueryString } from '../../../lib/tokyu-bus-timetable/parse-for-query-string/parse-for-query-string';
+import { fetchBusRoutesSelectHTML } from '../../../lib/tokyu-bus-timetable/bus-routes-select-html/fetch-bus-routes-select-html';
+import { fetchFinalQueryHTML } from '../../../lib/tokyu-bus-timetable/final-query-html/fetch-final-query-html';
+import { parseFinalQueryHTML } from '../../../lib/tokyu-bus-timetable/final-query-html/parse-final-query-html';
+import { fetchTimetableHTML } from '../../../lib/tokyu-bus-timetable/timetable-html/fetch-timetable-html';
+import { parseTimetableHtml } from '../../../lib/tokyu-bus-timetable/timetable-html/parse-timetable-html';
 
 
 const _router = new Router();
@@ -16,6 +23,44 @@ _router.use(async (ctx, next) => {
     ctx.body = makeWebApiErrorResultObject(e.message);
   }
 });
+
+
+/**
+ * First api to search busstop-name by a inputted word.
+ */
+_router.get('/busstops', async (ctx) => {
+  const search: string = ctx.query['search'];
+  const httpResult = await fetchBusStopNameSearchHTML(search);
+  // when busstop not found, result is an empty array [].
+  ctx.body = parseForQueryString(httpResult.contents);
+});
+
+
+/**
+ * Second api to get routes options by searched busstop-name.
+ */
+_router.get('/routes', async (ctx) => {
+  const queryString: string = ctx.query['query'];
+  const httpResult = await fetchBusRoutesSelectHTML(queryString);
+  // when bus-routes not found, result is an empty array [].
+  ctx.body = parseForQueryString(httpResult.contents);
+});
+
+
+/**
+ * Final api to get timetable data.
+ */
+_router.get('/', async (ctx) => {
+  const queryString: string = ctx.query['query'];
+  const httpResultOfFinalQuery = await fetchFinalQueryHTML(queryString);
+  // will throw an error when the fetched html is unexpected.
+  const queryObj = parseFinalQueryHTML(httpResultOfFinalQuery.contents);
+
+  const httpResultOfTimetable = await fetchTimetableHTML(queryObj);
+  // as above, will throw an error when the fetched html is unexpected.
+  ctx.body = parseTimetableHtml(httpResultOfTimetable.contents);
+});
+
 
 
 /**
