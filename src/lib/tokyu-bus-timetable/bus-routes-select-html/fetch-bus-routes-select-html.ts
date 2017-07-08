e8a7476fd08cbc0ed5c38d70fdab1d36cb363775
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2017-07-08 10:44:48 +0900

    refactor: replace a local var of url-string with a global constant

--- src/lib/tokyu-bus-timetable/bus-routes-select-html/fetch-bus-routes-select-html.ts
+++ src/lib/tokyu-bus-timetable/bus-routes-select-html/fetch-bus-routes-select-html.ts
@@ -1,12 +1,11 @@
 import { fetchSJISHtml } from '../../fetch-http/fetch-html';
 import { HttpResult } from '../../../types/index';
+import { TOKYU_BUS_TIMETABLE_URL } from '../../../config/constants';
 
-// endpoint
-const url = 'http://www.tokyubus.co.jp/keitai/i/index.cgi';
 
 /**
  * fetch next page of name-search page.
  */
 export function fetchBusRoutesSelectHTML(queryString: string): Promise<HttpResult> {
-  return fetchSJISHtml(`${url}?${queryString}`);
+  return fetchSJISHtml(`${TOKYU_BUS_TIMETABLE_URL}?${queryString}`);
 }
