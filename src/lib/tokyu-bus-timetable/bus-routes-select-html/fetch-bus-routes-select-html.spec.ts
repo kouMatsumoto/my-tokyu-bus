14e21dd1b17ec1e115abe8d8c5b0a3bfa5f188e2
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2017-07-09 01:02:43 +0900

    feat: update implementation of `fetchBusStopNameSearchHTML()`

--- src/lib/tokyu-bus-timetable/bus-routes-select-html/fetch-bus-routes-select-html.spec.ts
+++ src/lib/tokyu-bus-timetable/bus-routes-select-html/fetch-bus-routes-select-html.spec.ts
@@ -1,13 +1,28 @@
+import * as moment from 'moment';
 import { parseHTMLByAnchor } from '../parse-html-by-anchor/parse-html-by-anchor';
 import { useAsync } from '../../../../spec/support/helpers/use-async';
 import { fetchBusStopNameSearchHTML } from '../bus-stop-name-search-html/fetch-bus-stop-name-search-html';
 import { fetchBusRoutesSelectHTML } from './fetch-bus-routes-select-html';
 import { busRoutesSelectHTML } from './bus-routes-select-html.mock';
+import { retrieveFolderAndDispValue } from '../folder-and-disp-value-html/retrieve-folder-and-disp-value';
 
 
 describe('fetchBusRoutesSelectHTML', () => {
-  it('should fetch expected html', useAsync(async () => {
-    const searchHTTPResult = await fetchBusStopNameSearchHTML('下馬');
+  let options: any;
+  beforeEach((done) => {
+    retrieveFolderAndDispValue().then((retrieved) => {
+      options = {
+        mmdd: moment().format('MM/DD'),
+        folder: retrieved['folder'],
+        disp_history: retrieved['disp_history']
+      };
+      done();
+    });
+  });
+
+  // NOTE: this test can failed because routes options are variable by folder value.
+  xit('should fetch expected html', useAsync(async () => {
+    const searchHTTPResult = await fetchBusStopNameSearchHTML('下馬', options);
     const busstopNameData = parseHTMLByAnchor(searchHTTPResult.contents);
     const simouma1 = busstopNameData[0];
 
--- src/lib/tokyu-bus-timetable/test.spec.ts
+++ src/lib/tokyu-bus-timetable/test.spec.ts
@@ -1,3 +1,4 @@
+import * as moment from 'moment';
 import { useAsync } from '../../../spec/support/helpers/use-async';
 import { fetchBusStopNameSearchHTML } from './bus-stop-name-search-html/fetch-bus-stop-name-search-html';
 import { parseHTMLByAnchor } from './parse-html-by-anchor/parse-html-by-anchor';
@@ -7,11 +8,24 @@ import { TOKYU_BUS_TIMETABLE_URL } from '../../config/constants';
 import { parseFinalQueryHTML } from './final-query-html/parse-final-query-html';
 import { fetchTimetableHTML } from './timetable-html/fetch-timetable-html';
 import { parseTimetableHtml } from './timetable-html/parse-timetable-html';
+import { retrieveFolderAndDispValue } from './folder-and-disp-value-html/retrieve-folder-and-disp-value';
 
 
 describe('total test of tokyu-bus-timetable', () => {
-  xit('should conduct my-test', useAsync(async () => {
-    const html = await fetchBusStopNameSearchHTML('下馬');
+  let options: any;
+  beforeEach((done) => {
+    retrieveFolderAndDispValue().then((retrieved) => {
+      options = {
+        mmdd: moment().format('MM/DD'),
+        folder: retrieved['folder'],
+        disp_history: retrieved['disp_history']
+      };
+      done();
+    });
+  });
+
+  it('should conduct my-test', useAsync(async () => {
+    const html = await fetchBusStopNameSearchHTML('下馬', options);
     const parsed = parseHTMLByAnchor(html.contents);
     const simouma1 = parsed[0];
 
