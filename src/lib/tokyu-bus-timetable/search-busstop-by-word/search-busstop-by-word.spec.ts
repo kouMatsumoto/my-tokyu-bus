69942f048af27bd89843bf32e7cc67e8b81cfe66
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2017-07-24 23:49:53 +0900

    feat: implement a new function `searchBusstopByWord()`

---
+++ src/lib/tokyu-bus-timetable/search-busstop-by-word/search-busstop-by-word.spec.ts
@@ -0,0 +1,12 @@
+import * as expect from 'expect';
+import { searchBusstopByWord } from './search-busstop-by-word';
+import { useAsync } from '../../../../spec/helpers/use-async';
+
+
+describe('searchBusstopByWord', () => {
+  it('should fetch value properly', useAsync(async () => {
+    const searchWord = '下馬';
+    const actual = await searchBusstopByWord(searchWord);
+    expect(actual[0].name).toBe('下馬一丁目');
+  }));
+});
