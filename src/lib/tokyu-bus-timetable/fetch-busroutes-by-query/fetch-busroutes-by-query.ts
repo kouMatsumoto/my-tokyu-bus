2a772aaa5191e20bc55d60337ac7ad21218d6599
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2017-07-25 23:44:34 +0900

    feat: add a destination field to response data

--- src/lib/tokyu-bus-timetable/fetch-busroutes-by-query/fetch-busroutes-by-query.ts
+++ src/lib/tokyu-bus-timetable/fetch-busroutes-by-query/fetch-busroutes-by-query.ts
@@ -1,9 +1,24 @@
 import { parseHTMLByAnchor } from '../parse-html-by-anchor/parse-html-by-anchor';
 import { fetchBusRoutesSelectHTML } from './bus-routes-select-html/fetch-bus-routes-select-html';
+import { getDestinationFromBusroute } from './get-destination-from-busroute/get-destination-from-busroute';
 
 
-export async function fetchBusroutesByQuery(queryString: string) {
+interface FetchedBusrouteData {
+  name: string;
+  queryString: string;
+  destination: string;
+}
+
+
+export async function fetchBusroutesByQuery(queryString: string): Promise<FetchedBusrouteData[]> {
   const httpResult = await fetchBusRoutesSelectHTML(queryString);
-// when bus-routes not found, result is an empty array [].
-  return parseHTMLByAnchor(httpResult.contents);
+  // when bus-routes not found, result is an empty array [].
+  const busroutesData = parseHTMLByAnchor(httpResult.contents);
+
+  return busroutesData.map((data) => {
+    return {
+      ...data,
+      destination: getDestinationFromBusroute(data.name)
+    };
+  });
 }
