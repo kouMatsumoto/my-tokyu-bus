9a208e0a83880f62931ce65d682303951fdad767
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2017-08-01 22:37:37 +0900

    feat: implement fetchBusLocationHTML

---
+++ src/lib/tokyu-bus-location/fetch-bus-location/fetch-bus-location-html/fetch-bus-location-html.ts
@@ -0,0 +1,22 @@
+import { fetchHtml } from '../../../common/http/fetch-html';
+
+const url = 'http://tokyu.bus-location.jp/blsys/navi';
+const essentialParams = {
+  'VID': 'lsc',
+  'EID': 'nt',
+  'SCT': '2',
+};
+
+
+export async function fetchBusLocationHTML(departureId: string, arrivalId: string) {
+  const params = {
+    ...essentialParams,
+    DSMK: departureId,
+    ASMK: arrivalId,
+  };
+  const httpResult = await fetchHtml(url, params);
+
+  // Todo: implement error handling.
+
+  return httpResult.contents;
+}
