1c6488032578516e97cd8f0b8689b88139a3f0f2
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2017-08-02 23:32:01 +0900

    feat: implement parseBusLocationHTML

--- src/lib/tokyu-bus-location/fetch-bus-location/parse-bus-location-html/parse-bus-location-html.spec.ts
+++ src/lib/tokyu-bus-location/fetch-bus-location/parse-bus-location-html/parse-bus-location-html.spec.ts
@@ -1,12 +1,24 @@
-import { useAsync } from '../../../../../spec/helpers/use-async';
 import * as expect from 'expect';
 import { parseBusLocationHTML } from './parse-bus-location-html';
 import { busLocationHTMLMock } from './bus-location-html.mock';
 
 
+const expected = [
+ {
+   name: '渋３２ 渋谷駅行',
+   waitTime: 0,
+   remainingStops: 0
+ },
+ {
+   name: '渋３２ 渋谷駅行',
+   waitTime: 6,
+   remainingStops: 4
+ }
+];
+
 describe('parseBusLocationHTML', () => {
-  it('should', useAsync(async () => {
-    const actual = await parseBusLocationHTML(busLocationHTMLMock);
-    expect(actual).toBeTruthy();
-  }));
+  it('should parse properly', () => {
+    const actual = parseBusLocationHTML(busLocationHTMLMock);
+    expect(actual).toEqual(expected);
+  });
 });
--- src/lib/tokyu-bus-location/fetch-bus-location/parse-bus-location-html/parse-bus-location-html.ts
+++ src/lib/tokyu-bus-location/fetch-bus-location/parse-bus-location-html/parse-bus-location-html.ts
@@ -1,12 +1,19 @@
+import { filterLocationDataFromText } from './filter-location-data-from-text/filter-location-data-from-text';
+
 const jsdom = require('jsdom');
 const { JSDOM } = jsdom;
 
-interface TokyuBusInformation {
-  waitTimes: number;
+interface BusLocationData {
+  name: string;
+  waitTime: number;
+  remainingStops: number;
 }
 
-
-const needle = 'td.businfo em';
+const mainTableSelector = 'table.sekkinListTbl';
+const evenTrSelector = '.trEven';
+const busInfoTdSelector = 'td.businfo';
+// how many odd-even-tr-sets exists in table.
+const rowCount = 6;
 
 /**
  * Parse html of tokyu-bus-navi
@@ -17,43 +24,29 @@ const needle = 'td.businfo em';
 export function parseBusLocationHTML(html: string) {
   const dom = new JSDOM(html);
 
-  const businfoElms = dom.window.document.querySelectorAll(needle);
+  const mainTableElm = dom.window.document.querySelector(mainTableSelector);
+  const evenTrElms = <HTMLTableRowElement[]>Array.from(mainTableElm.querySelectorAll(evenTrSelector));
 
-  const infoArray: TokyuBusInformation[] = [];
-  for (let elm of businfoElms) {
-    const info = parseTextOfElement(elm.textContent);
-    if (info) {
-      infoArray.push(info);
+  const busInfoData: BusLocationData[] = [];
+  for (let i = 0; i < rowCount; i++) {
+    const trElm = evenTrElms[i];
+    const tdElm = trElm.querySelector(busInfoTdSelector);
+    // null check
+    if (!tdElm) {
+      continue;
     }
+
+    const filteredData = filterLocationDataFromText(tdElm.textContent);
+    // null check
+    if (filteredData === null) {
+      continue;
+    }
+
+    busInfoData.push({
+      ...filteredData,
+      remainingStops: i
+    });
   }
 
-  // sort `infoArray` by `waitTimes` asc
-  if (0 < infoArray.length) {
-    infoArray.sort((a, b) => a.waitTimes - b.waitTimes);
-  }
-
-  return infoArray;
-}
-
-
-/**
- * To parse dd tag contents of tokyu-bus-navi page html
- *
- * @param text {string} - e.g. '下馬一行 12分待ち', '(折)下馬一行 ', '渋谷駅行 ', '下馬一行 02分待ち'
- * @returns {TokyuBusInformation}
- */
-function parseTextOfElement(text: string): TokyuBusInformation | null {
-  const regexp = /([0-9]+)分待ち/iu;
-  const matchResult = text.match(regexp); // e.g. ["12分待ち","12"], null
-
-  // when bus is coming
-  if (Array.isArray(matchResult)) {
-    return {
-      waitTimes: parseInt(matchResult[1])
-    };
-
-    // when no bus is coming
-  } else {
-    return null;
-  }
+  return busInfoData;
 }
