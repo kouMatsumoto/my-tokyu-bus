b5979f13d12d057c53da7f950f2a3466bd29097d
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2017-08-03 20:32:22 +0900

    feat: implement fetchBusLocationByBusstopNames()

--- src/lib/tokyu-bus-location/fetch-bus-location-by-busstop-names/fetch-bus-location-by-busstop-names.ts
+++ src/lib/tokyu-bus-location/fetch-bus-location-by-busstop-names/fetch-bus-location-by-busstop-names.ts
@@ -1,6 +1,20 @@
 import { fetchBusstopIds } from '../fetch-busstop-ids/fetch-busstop-ids';
+import { fetchBusLocation } from '../fetch-bus-location/fetch-bus-location';
+import { BusLocationData } from '../../../types/bus-location-data';
 
-export async function fetchBusLocationByBusstopNames(departure: string, arrival: string) {
+export async function fetchBusLocationByBusstopNames(departure: string, arrival: string): Promise<BusLocationData[]> {
   const data = await fetchBusstopIds(departure, arrival);
-  return data;
+
+  // validate fetched data
+  if (data.departures.length === 0) {
+    throw new Error('departure busstop is not found');
+  }
+  if (data.arrivals.length === 0) {
+    throw new Error('arrival busstop is not found');
+  }
+
+  const departureId = data.departures[0].value;
+  const arrivalId = data.arrivals[0].value;
+
+  return await fetchBusLocation(departureId, arrivalId);
 }
