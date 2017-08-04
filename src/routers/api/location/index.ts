9dc1ca7b4336cd11b31f0bec2b4cd10cb2861034
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2017-08-04 22:10:00 +0900

    feat: unify formatting api-response schema

--- src/routers/api/location/index.ts
+++ src/routers/api/location/index.ts
@@ -1,23 +1,8 @@
 import * as Router from 'koa-router';
-import { makeWebApiErrorResultObject } from '../../../lib/api-response/make-web-api-error-result-object';
 import { fetchBusLocationByBusstopNames } from '../../../lib/tokyu-bus-location/fetch-bus-location-by-busstop-names/fetch-bus-location-by-busstop-names';
 
 
 const _router = new Router();
-
-/**
- * Error handling for this router
- * it's better move this error handling to global router
- */
-_router.use(async (ctx, next) => {
-  try {
-    await next();
-  } catch (e) {
-    ctx.status = e.status || 500;
-    ctx.body = makeWebApiErrorResultObject(e.message);
-  }
-});
-
 
 /**
  * Final api to get timetable data.
