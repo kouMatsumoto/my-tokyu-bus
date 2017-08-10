10523440b15aa2ed2a6550c2b8c1ff35d702dd4f
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2017-08-10 22:02:28 +0900

    refactor: remove build files because built files are outputted into src

--- build/lib/get-message-from-information.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function getMessageFromInformation(information) {
-    return `Next bus is ${information.waitTimes} minutes away`;
-}
-exports.getMessageFromInformation = getMessageFromInformation;
--- build/lib/make-web-api-result-object.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function makeWebApiResultObject(informations) {
-    return {
-        data: informations
-    };
-}
-exports.makeWebApiResultObject = makeWebApiResultObject;
