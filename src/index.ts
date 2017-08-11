ff15ce1a6492b39aa50126e300ee65b3baff6bb6
kouMatsumoto <kou.matsumoto.jp@gmail.com>
2017-08-11 20:22:35 +0900

    feat: disable static file serving

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
 import * as Koa from 'koa';
 import * as koaBodyparser from 'koa-bodyparser';
-import * as koaStatic from 'koa-static';
 import * as kcors from 'kcors';
 import { rootRouter } from './routers';
-import { PUBLIC_ROOT } from './config/index';
 import { processTimeLogger } from './middlewares/process-time-logger';
 import { devLogger } from './lib/logger/dev-logger';
 
@@ -13,8 +11,6 @@ const app = new Koa();
  * Update header for Cross-Origin Resource Sharing
  */
 app.use(kcors());
-
-app.use(koaStatic(PUBLIC_ROOT));
 app.use(koaBodyparser());
 
 
